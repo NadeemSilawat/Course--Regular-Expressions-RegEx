9b9cf8fde8756ad80abaefb7884683825f965215
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-05-26 09:30:55 +0530

    Added Regex Exercise

--- assets/validation.js
+++ assets/validation.js
@@ -1,4 +1,23 @@
-var regx = /[0-9]+/gi;
+const inputs = document.querySelectorAll('input');
 
-var vale = 
-console.log(username)
+const pattern = {
+  telephone: /^\d{11}$/,
+};
+
+function validate(field, regex){
+
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+
+}
+
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', (e) => {
+    // console.log(e.target.attributes.name.value);
+    validate(e.target,pattern[e.target.attributes.name.value]);
+  });
+});
